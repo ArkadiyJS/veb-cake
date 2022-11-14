e1ee9a5ad410b9bf465240824f461cc5f8233108
Arkadiy Vologodskiy <vologodskiy20@gmail.com>
2022-11-14 21:22:30 +0500

    add loyaut components in main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ function App() {
   return (
     <div className="App">
       Hi project veb tortik
+      <div className='Header'> header info </div>
+
+
+      <div className='NavBar'> 
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div>
+      <div> sortirovka</div> 
+      </div>
+
+
+      <div className='Content'>
+
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+
+
+      <div><img alt='cake' src='https://sun9-84.userapi.com/impg/SMTnBUZ1arGjMBBLoWDXgrngr63V9H2ue43liQ/z2s_OQYmv9I.jpg?size=1280x960&quality=95&sign=970d6f5af1d4e0c15ae7c6c7dc64709c&type=album'/></div>
+      </div>
+
+      
     </div>
   );
 }

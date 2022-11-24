0c1ed760c0ea4e8f49c5b2bde460526d486e7f2b
Arkadiy Vologodskiy <vologodskiy20@gmail.com>
2022-11-25 00:30:21 +0500

    base setting routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-
 import { useState } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 import CardCake from './components/cardCake';
 import PageInfo from './components/pageInfo';
 import CategoryCake from './components/categoryCake';
 import Page404 from './components/page404';
+import { NavLink, Route,  Routes } from 'react-router-dom';
 
 
 
@@ -24,20 +20,34 @@ function App() {
   return (
     <div className="App">
 
-      <div className='Header'> header info </div>
+      <div className='Header'> 
+
+        <NavLink to='/info'>как заказать?</NavLink>
+         header info
+      
+      </div>
 
 
       <div className='NavBar'>
-        <CategoryCake />
 
+         <CategoryCake /> 
+        
       </div>
 
 
       <div className='Content'>
 
-        <CardCake modalShow={modalShow} setModalShow={setModalShow} />
-        {/* <PageInfo /> */}
-        {/* <Page404/> */}
+
+        <Routes>
+
+          <Route path='/' element={<CardCake modalShow={modalShow} setModalShow={setModalShow} />}/>
+
+          <Route path='/info' element={<PageInfo />}/>
+
+          <Route path='*' element={<Page404/>}/>
+
+        </Routes>
+        
 
       </div>
 

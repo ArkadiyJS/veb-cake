4057e88399e2e058ae158f172e53d0fc9386013c
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-07-26 11:41:11 +0500

    ref

--- src/components/categoryCake.jsx
+++ src/components/categoryCake.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 function CategoryCake(props) {
   return (
-    <>
+    <div className='Cate'>
       {/*  переделать кнопки */}
       <Button variant="primary">Всё</Button>{' '}
       <Button variant="secondary">Торты</Button>{' '}
@@ -13,7 +13,7 @@ function CategoryCake(props) {
       <Button variant="info">Пироженое</Button>{' '}
       <Button variant="light">Кэнди бар</Button>{' '}
 
-    </>
+    </div>
   );
 }
 

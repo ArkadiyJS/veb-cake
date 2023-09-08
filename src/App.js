36a16b23aa6319af575be4b5c5edc2ac047217dd
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-09-08 19:08:39 +0500

    add test data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import PageInfo from './components/pageInfo';
 import CategoryCake from './components/categoryCake';
 import Page404 from './components/page404/page404';
 import { NavLink, Route, Routes } from 'react-router-dom';
+import NavBar from './components/navBar/navBar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 
 
@@ -24,15 +28,14 @@ function App() {
       <div className='Header'>
 
 
-        <NavLink to='/info'>как заказать?</NavLink>
-        header info
+
 
       </div>
 
 
       <div className='NavBar'>
 
-        <CategoryCake />
+        <NavBar />
 
       </div>
 
@@ -40,14 +43,196 @@ function App() {
       <div className='Content'>
 
 
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://sun9-31.userapi.com/impg/vraWsJU26-xFSxno9wgAceigg1t0LcHr0LW15A/oXlH8eRq_7Y.jpg?size=1042x1600&quality=95&sign=d06ecdb8fac25471cf33b9b12ca88d80&type=album" />
+          <Card.Body>
+            <Card.Title>Торт "Красный Бархат"</Card.Title>
+            <Card.Text>
+              Декор: сахарная фотопечать, конфеты "Ferrero Rocher", топпер.
+
+              Вес: 2 кг.
+              Стоимость: 3100 руб.
+
+              ❗На первый заказ скидка - 10%
+              для заказа пишите в ЛС
+            </Card.Text>
+
+          </Card.Body>
+        </Card>
+
+
+
+
+
+
 
         <Routes>
 
           {/* <Route path='/' element={<CakeElement modalShow={modalShow} setModalShow={setModalShow} />} /> */}
 
-          <Route path='/info' element={<PageInfo />} />
+          {/* <Route path='/info' element={<PageInfo />} /> */}
 
-          <Route path='*' element={<Page404 />} />
+          {/* <Route path='*' element={<Page404 />} /> */}
 
         </Routes>
 

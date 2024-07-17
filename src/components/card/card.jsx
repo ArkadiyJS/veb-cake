866e630ad543a711b52d1061b4ec2fffbbe1aac4
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-07-17 06:21:51 +0500

    add: companents(html)

---
+++ src/components/card/card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div className='card'>
+      <img alt='' src='' />
+      <div className='bodyCard'>
+        <div className='titleCard'>
+
+        </div>
+        <p className='textCard'>
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;

b67ddac3809fb4a39a189cfad7e2c227bf3573c1
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-08-06 19:09:27 +0500

    skelet css html

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import Card from './components/card/card'
 
 function App() {
 
 
   return (
-    <>
+    <div className='App'>
+      {/* <div className='nav-bar'></div>
 
-    </>
+      <div className='main'></div>
+
+      <div className='footer'></div> */}
+      <Card /> <Card /><Card />
+      <Card /><Card /><Card />
+      <Card /><Card /><Card />
+    </div>
   )
 }
 
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import s from './card.module.css'
 
 function Card(props) {
   return (
-    <div className='card'>
-      <img alt='' src='' />
-      <div className='bodyCard'>
-        <div className='titleCard'>
 
-        </div>
-        <p className='textCard'>
-
+    <div className={s.card}>
+      <img className={s.cardImg} alt='cake' src='https://sun9-23.userapi.com/impg/FA-BDB8uozz0D5n8W2hn1TMr94S0jRsCZ9NXtg/Zsy08ZaSTxo.jpg?size=1280x1228&quality=95&sign=0d8effb191fc95630564ec288f037e46&type=album' />
+      <div className={s.bodyCard}>
+        <h3 className={s.titleCard}>
+          торт "Красный бархат"
+        </h3>
+        <p className={s.textCard}>
+          тортик по хорошей и вкусной цене
         </p>
       </div>
     </div>
